7625077d6f4bd2b003f0c68fba77075d11d5ec86
Alvin <alvinngg9@gmail.com>
2024-08-20 17:02:54 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,12 +4,18 @@ import java.util.Arrays;
 
 public class Solution {
     public int lengthOfLIS(int[] nums) {
+        //create a new int array with the same size as nums array
         int[] array = new int[nums.length];
+        //fill the array with 1s
         Arrays.fill(array, 1);
         int result = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] < nums[j]) {
+        //iterate through the given array
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                //check if indexes before i is less than index i
+                if (nums[i] > nums[j]) {
+                    //increase the value at index i by 1 for the new array whenever the value of
+                    //the nums array at index j is less than index i
                     array[i] = Math.max(array[i], array[j] + 1);
                     result = Math.max(array[i], result);
                 }
